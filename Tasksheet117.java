1a91e37e3e821cefdd7effca78d656a180ed671b
Lancerokobe <141099012+Lancerokobe@users.noreply.github.com>
2024-08-21 15:03:21 +0000

    Indentifying if all equal and largest number

--- Tasksheet117.java
+++ Tasksheet117.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class Tasksheet117 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 3 numbers:");
+        int inp1 = scan.nextInt();
+        int inp2 = scan.nextInt();
+        int inp3 = scan.nextInt();
+        System.out.println();
+        
+        if (inp1 == inp2 && inp1 == inp3){
+            System.out.println("All numbers are equal");
+        }
+        
+        if (inp1 > inp2 && inp1 > inp3) {
+            System.out.println("The largest number is: "+ inp1);
+        } else if (inp2 > inp1 && inp2 > inp3){
+            System.out.println("Ther largest number is: "+ inp2);
+        } else if (inp3 > inp1 && inp3 > inp2) {
+            System.out.println("The largest number is: "+ inp3);
+        } 
+    }
+}
